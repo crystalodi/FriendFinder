2f465e93580224d9b58d8dbaa68339782e5f2b68
Crystal O <crystalodi@gmail.com>
2018-04-29 19:03:47 -0500

    change jquery include. calculate best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,24 @@ module.exports = function(app) {
 
   app.post("/api/friends", function(req, res){
     var newFriend = req.body;
+    var newFriendScores = newFriend.scores;
+    //Perform search for best friend and send best friend to call back function.
+    var smallestIndex = 0;
+    var compatibilityScoresArr = [];
+    for(var i = 0; i < friendsArray.length; i++) {
+      var friendScores = friendsArray[i].scores;
+      var compatibilitySum = 0;
+      for(var x = 0; x < friendScores.length; x++) {
+        compatibilitySum += Math.abs(friendScores[x] - newFriendScores[x])
+      }
+      compatibilityScoresArr.push(compatibilitySum)
+    }
+    for(var i = 0; i < compatibilityScoresArr.length; i++) {
+      if(compatibilityScoresArr[i] < compatibilityScoresArr[smallestIndex]) {
+        smallestIndex = i;
+      }
+    }
     friendsArray.push(newFriend);
-    res.send(true);
+    res.send(friendsArray[smallestIndex]);
   })
 }
