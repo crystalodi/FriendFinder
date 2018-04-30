0551450003cf726d4695c598615a29c30dfb51d5
Crystal O <crystalodi@gmail.com>
2018-04-29 23:20:19 -0500

    Add header to pages. Update scores array.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 var friendsArr = [
   {
     "name": "Crystal",
-    "photo": "https://placeimg.com/320/240/any",
-    "scores": [5, 3, 3, 5, 2, 1, 5, 5, 5, 1]
+    "photo": "https://placeimg.com/640/480/any",
+    "scores": ["5", "3", "3", "5", "2", "1", "5", "5", "5", "1"]
   }
 ]
 
